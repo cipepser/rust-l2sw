713b836ae99a4812ad88f563c88909f0ed7c7997
cipepser <respepic@gmail.com>
2018-11-26 13:01:41 +0900

    fix warning

--- pcap/src/main.rs
+++ pcap/src/main.rs
@@ -4,10 +4,32 @@ use std::collections::HashSet;
 use std::thread;
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
-use pnet::datalink::
+use pnet::packet::ethernet::EthernetPacket;
 
-fn reveive_packet(rx: &mut Box<DataLinkReceiver>) {
+fn receive_packet(interface: &NetworkInterface, rx: &mut Box<datalink::DataLinkReceiver>) -> Result<(), String> {
+    loop {
+        let next_packet = rx.next()
+            .map_err(|e| format!("An error occurred when read next packet: {}", e.to_string()))
+            .and_then(|packet| {
+                EthernetPacket::new(packet).ok_or("failed to parse ethernet packet".to_string())
+            });
 
+        match next_packet {
+            Ok(packet) => {
+                println!(
+                    "{}: {}, {} > {}",
+                    interface.name,
+                    packet.get_ethertype(),
+                    packet.get_source(),
+                    packet.get_destination(),
+                );
+            }
+            Err(err) => {
+                println!("failed to read next packet {}, ignore and continue.", err);
+                continue;
+            }
+        }
+    }
 }
 
 fn main() {
@@ -23,15 +45,20 @@ fn main() {
     let handles: Vec<_> = interfaces.into_iter()
         .map(|interface|
             thread::spawn(move || {
-                let mut rx = datalink::channel(&interface, Default::default())
-                    .map(|chan|
+                let rx = datalink::channel(&interface, Default::default())
+                    .map(|chan| match chan {
                         Ethernet(_, rx) => rx,
-                        _ => panic!("failed to initialize datalink channel {:?}", interface.name),
-                    );
-                reveive_packet(&mut rx);
+                        _ => panic!("could not initialize datalink channel {:?}", interface.name),
+                    });
+                match receive_packet(&interface, &mut rx.unwrap()) {
+                    Ok(_) => (),
+                    Err(e) => panic!("{}", e.to_string()),
+                };
             })
         )
         .collect();
 
-    let _ = handles.into_iter().map(|h| h.join());
+    for h in handles {
+        h.join().unwrap();
+    }
 }
