fd7779e22f8ff3bc5c497d6d6fc585019a6d7b8d
cipepser <respepic@gmail.com>
2018-12-26 00:18:45 +0900

    templary commit

--- l2sw/src/main.rs
+++ l2sw/src/main.rs
@@ -2,83 +2,127 @@ extern crate pnet;
 
 use std::collections::{HashSet, HashMap};
 use std::thread;
+use std::sync::mpsc;
 use std::sync::{Mutex, Arc};
-use std::time::Duration;
-use std::io;
+use std::ops::Deref;
 use pnet::datalink::{self, NetworkInterface};
-use pnet::datalink::Channel;
 use pnet::datalink::Channel::Ethernet;
-use pnet::packet::ethernet::EthernetPacket;
-//use pnet::util;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::{Packet, arp, tcp, udp};
 
-// receive_packetの責務が大きすぎる？
-// - interfaceからrxを取得 <- これはテーブルのlockを取る必要がない
-//   ⇛ このあとのことを考えるとchannel(tx, rx)のどちらも持っておきたい
-// - rxを監視して、パケットがあればそれを処理する
-fn receive_packet(interface: &NetworkInterface, mac_address_table: &mut HashMap<pnet::datalink::MacAddr, MacAddressRecord>) -> Result<(), String> {
-//fn receive_packet(interface: &NetworkInterface, ) -> Result<(), String> {
-    println!("name: {:?}", interface.name);
+#[derive(Clone, Debug)]
+struct Table<T> {
+    inner: Arc<Mutex<T>>,
+}
 
-    let mut rx = datalink::channel(&interface, Default::default())
-        .map(|chan| match chan {
-            Ethernet(_, rx) => rx,
-            _ => panic!("Unhandled channel type"),
-        })
-        .map_err(|e| {
-            format!("An error occurred when creating the datalink channel: {}",
-                    e.to_string()
-            )
-        })?;
+struct Record<T> {
 
-    loop {
-        let next_packet = rx.next()
-            .map_err(|e| format!("An error occurred when read next packet: {}", e.to_string()))
-            .and_then(|packet| {
-                EthernetPacket::new(packet).ok_or("failed to parse ethernet packet".to_string())
-            });
+}
 
-        match next_packet {
-            Ok(packet) => {
-                println!(
-                    "{}: {} -> {}",
-                    interface.name,
-//                    packet.get_ethertype(),
-                    packet.get_source(),
-                    packet.get_destination(),
-                );
-                // TODO: ここで初めてRecordに対するlockを取りたい
-                // ⇛ receive_packetの引数にmac_address_tableがあることが設計が間違っている？
-                // MacAddressTable型に対して`update`とかを呼び出すべきでは？
-                // でもこの関数内ではMacAddressTableのことを知らないので、どうにかして知る必要がある
-//                let _record = mac_address_table[&packet.get_source()].lock(); // .unwrap()が例にはあるけど、Resultで返したい
-//                update_MacAddressTable(&mut mac_address_table[&packet.get_source()]);
-//                handle_packet(&interface, &packet);
-//                println!("{} updated", packet.get_source());
-            }
-            Err(err) => {
-                println!("failed to read next packet {}, ignore and continue.", err);
-                continue;
-            }
+impl<T: Send> Queue<T> {
+    fn new() -> Self {
+        Self { inner: Arc::new(Mutex::new(VecDeque::new())) }
+    }
+
+    fn get(&self) -> Option<T> {
+        let _queue = self.inner.lock();
+        if let Ok(mut queue) = _queue {
+            queue.pop_front()
+        } else {
+            None
         }
     }
 
-//    Ok(())
-}
-
-struct MacAddressRecord {
-    device_no: u32,
-//    last_time: Duration,
-}
-
-impl MacAddressRecord {
-    fn update_divice_no(&mut self, device_no: u32) {
-        self.device_no = device_no;
+    fn add(&self, obj: T) -> usize {
+        let _queue = self.inner.lock();
+        if let Ok(mut queue) = _queue {
+            queue.push_back(obj);
+            queue.len()
+        } else {
+            0
+        }
     }
 }
 
+
+#[derive(Clone, Debug)]
+struct PacketWithInterface {
+    interface: NetworkInterface,
+    packet: Vec<u8>,
+}
+
+fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
+    println!(
+        "{}: {} > {}",
+        interface.name,
+        ethernet.get_source(),
+        ethernet.get_destination(),
+    );
+
+    print!("  {}: ", ethernet.get_ethertype());
+    match ethernet.get_ethertype() {
+        EtherTypes::Arp => {
+            let arp = arp::ArpPacket::new(ethernet.payload()).unwrap();
+            match arp.get_operation() {
+                arp::ArpOperations::Reply => {
+                    println!(
+                        "ARP reply({}): {} -> {}",
+                        arp.get_sender_proto_addr(),
+                        arp.get_sender_hw_addr(),
+                        arp.get_target_hw_addr()
+                    );
+                }
+                arp::ArpOperations::Request => {
+                    println!(
+                        "ARP request({}): {} -> {}",
+                        arp.get_target_proto_addr(),
+                        arp.get_sender_hw_addr(),
+                        arp.get_target_hw_addr()
+                    );
+                }
+                _ => (),
+            }
+        }
+        EtherTypes::Ipv4 => {
+            let ip = Ipv4Packet::new(ethernet.payload()).unwrap();
+            println!("{} -> {}", ip.get_source(), ip.get_destination());
+            handle_ip_packet(&interface, &ip)
+        }
+        _ => (),
+    }
+}
+
+fn handle_arp_packet(interface: &NetworkInterface, arp: arp::ArpPacket) {
+
+}
+
+fn handle_ip_packet(interface: &NetworkInterface, ip: &Ipv4Packet) {
+    print!("    {}: ", ip.get_next_level_protocol());
+    match ip.get_next_level_protocol() {
+        IpNextHeaderProtocols::Tcp => {
+            let tcp = tcp::TcpPacket::new(ip.payload()).unwrap();
+            handle_tcp_packet(&interface, &tcp);
+        }
+        IpNextHeaderProtocols::Udp => {
+            let udp = udp::UdpPacket::new(ip.payload()).unwrap();
+            handle_udp_packet(&interface, &udp);
+        }
+        _ => (),
+    }
+}
+
+fn handle_tcp_packet(_interface: &NetworkInterface, tcp: &tcp::TcpPacket) {
+    println!("{} -> {}", tcp.get_source(), tcp.get_destination());
+}
+
+fn handle_udp_packet(_interface: &NetworkInterface, udp: &udp::UdpPacket) {
+    println!("{} -> {}", udp.get_source(), udp.get_destination());
+}
+
 fn main() {
-    let interface_names: HashSet<&str> = vec!["lo0", "en0", "en1"]
-//    let interface_names: HashSet<&str> = vec!["en0"]
+    let interface_names: HashSet<&str> = vec!["lo0", "en0"]
         .into_iter()
         .collect();
 
@@ -87,71 +131,60 @@ fn main() {
         .filter(|interface: &NetworkInterface| interface_names.contains(interface.name.as_str()))
         .collect();
 
-    // TODO: Arc(Mutex())するのをMacAddressRecordごとにしたい
-    let mac_address_table: Arc<Mutex<HashMap<pnet::datalink::MacAddr, MacAddressRecord>>> =
-        Arc::new(Mutex::new(HashMap::new()));
+    let (sender, receiver) = mpsc::channel();
 
-//    let mut channels: Vec<Result<Channel, String>> = interfaces
-//        .into_iter()
-//        .map(|interface|
-//            datalink::channel(&interface, Default::default())
-//                .map_err(|e| {
-//                    format!("An error occurred when creating the datalink channel: {}",
-//                            e.to_string()
-//                    )
-//                })
-//        )
-//        .collect();
+    let mut handles: Vec<_> = interfaces.into_iter()
+        .map(|interface| {
+            let sender = sender.clone();
+            thread::spawn(move || {
+                let mut rx = datalink::channel(&interface, Default::default())
+                    .map(|chan| match chan {
+                        Ethernet(_, rx) => rx,
+                        _ => panic!("could not initialize datalink channel {:?}", interface.name),
+                    }).unwrap();
 
-    let mut channels: Vec<Channel> = interfaces
-        .into_iter()
-        .map(|interface|
-    let chan = match datalink::channel(&interface,
-                                       Default::default())
-        {
-            Ok(chan) => chan,
-            Err(err) => println!("An error occured when creating channel but ignore: {}", err),
-        };
-    )
-    .collect();
+                loop {
+                    match rx.next() {
+                        Ok(src) => {
+                            sender.send(PacketWithInterface {
+                                interface: interface.clone(),
+                                packet: src.to_owned(),
+                            }).unwrap();
 
-    // 送信用のバッファを初期化
-    // - どのI/Fのtxに送るのか
+                        }
+                        Err(_) => {
+                            continue;
+                        }
+                    }
+                }
+            })
+        }
+        )
+        .collect();
+
+    handles.push(thread::spawn(move || {
+        loop {
+            match receiver.recv() {
+                Ok(packet_with_interface) => {
+                    let _packet = packet_with_interface.packet.deref();
+                    match EthernetPacket::new(_packet) {
+                        Some(packet) => {
+                            handle_ethernet_frame(&packet_with_interface.interface, &packet);
+                        },
+                        _ => {
+                            continue;
+                        }
+                    }
+                },
+                _ => {
+                    continue;
+                }
+            }
+        }
+    }));
 
 
-//    // TODO: 受信したらMACアドレステーブルを更新
-//    // 最初はブロードキャストするだけでいい？
-//    // ARP requestを投げる？
-//    let handles: Vec<_> = interfaces
-//        .into_iter()
-//        .map(|interface| {
-//            let mut mac_address_table = mac_address_table.clone();
-//
-//            thread::spawn(move || {
-//                // TODO: 今のままだと、loopの外でlockを取っているのでunlockされず、blockしてしまう
-//                // receive_packet()の返り値をResult<MacAddressRecord, String>にすることでlockを取らなくてよくできないか？
-//                // rxのloopとの切り離しが設計できていないからちょっと厳しそう
-//                // rxの取得自体はマルチスレッドにする必要はない
-//                let mut mac_address_table = mac_address_table.lock().unwrap();
-//                match receive_packet(&interface, &mut mac_address_table) {
-////                match receive_packet(&interface) {
-//                    Ok(_record) => println!("ok in main"),
-//                    Err(e) => panic!(e),
-//                };
-//            })
-//        })
-//        .collect();
-//
-////    let mac_address_table = mac_address_table.clone();
-//    for h in handles {
-////        let mac_address_table = mac_address_table.clone();
-//        h.join().unwrap();
-//    }
-//
-//    // tx: バッファからパケットを取り出して送信？
-//    //     これを実現しようとするとblockが起きる？
-//
-//    // 各テーブルのaging timerをいつ更新するか
-//    // ⇛学習したタイミングでlastTimeを更新する
-//    // テーブルを調べたときに now > lastTimeでもう一度ブロードキャストするか決める
+    for h in handles {
+        h.join().unwrap();
+    }
 }
